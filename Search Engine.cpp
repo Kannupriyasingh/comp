b59bfa4f4cccefb9e15919d7579a72302e3b538b
Kannu Priya <44132532+Kannupriyasingh@users.noreply.github.com>
2021-01-11 22:47:23 +0530

    Update and rename Search Engine to Search Engine.cpp

--- Search Engine.cpp
+++ Search Engine.cpp
@@ -1,4 +1,4 @@
-*********************************************************************************************************************************************************************************
+/*********************************************************************************************************************************************************************************
 Let us see how search engines work. Consider the following simple auto complete feature. When you type some characters in the text bar, the engine automatically gives best
 matching options among it's database. Your job is simple. Given an incomplete search text, output the best search result. Each entry in engine's database has a priority factor
 attached to it. We consider a result / search suggestion best if it has maximum weight and completes the given incomplete search query. For each query in the input, print the
@@ -20,7 +20,8 @@ hackerrank 9
 hacker
 SAMPLE OUTPUT
 10
-*********************************************************************************************************************************************************************************
+
+*********************************************************************************************************************************************************************************/
 #include <bits/stdc++.h>
 using namespace std;
 
